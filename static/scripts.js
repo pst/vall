5093443ff71268f1b7aa8808145e51c00c71c99c
Fabio Bacigalupo <fabio@dostoaster.(none)>
2013-05-04 16:09:03 +0200

    First version with working local video

--- static/scripts.js
+++ static/scripts.js
@@ -1,17 +1,26 @@
+$(document).ready(function() {
+
   var onFailSoHard = function(e) {
     console.log('Reeeejected!', e);
   };  // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
 
   function onSuccess(stream) {   
     var video = $('#ownVideo');
-    video.src = window.URL.createObjectURL(stream);
+    video.get(0).src = window.URL.createObjectURL(stream);
 
     // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
     // See crbug.com/110938.
     video.onloadedmetadata = function(e) {
       // Ready to go. Do some stuff.
-      console.log(e);
     };  
+
+    var canvas = $('#videoCanvas').get(0);
+    var context = canvas.getContext('2d');
+    var cw = canvas.clientWidth;
+    var ch = canvas.clientHeight;  
+    canvas.width = cw;
+    canvas.height = ch;
+    draw(video.get(0),context,cw,ch);    
   }
 
   window.URL = window.URL || window.webkitURL;
@@ -24,20 +33,19 @@
     alert('Sorry, your browser does not support vall.me');
   }
 
-/*var v = $('#ownVideo');
-var canvas = $('#videoCanvas');
-var context = canvas[0].getContext('2d');
-var cw = Math.floor(canvas[0].clientWidth / 100);
-var ch = Math.floor(canvas[0].clientHeight / 100);
-canvas.width = cw;
-canvas.height = ch;
 
-v.bind('play', function(){
-    draw(this,context,cw,ch);
-},false);
+/*  video.bind('play', function(){
+    console.log("Hallo");
+      draw(this,context,cw,ch);
+  },false);
+*/
+  function draw(v,c,w,h) {
+      //if(v.paused || v.ended) return false;
 
-function draw(v,c,w,h) {
-    if(v.paused || v.ended) return false;
-    c.drawImage(v,0,0,w,h);
-    setTimeout(draw,20,v,c,w,h);
-}*/
+      if(v.paused) {
+        v.play();
+      } 
+      c.drawImage(v,0,0,w,h);
+      setTimeout(draw,100,v,c,w,h);
+  }
+}); 

03e7852d106a4ce7cbe5a526910eddcf17b4820c
Philipp Strube <pst@cloudcontrol.de>
2013-05-05 13:03:40 +0200

    debugging

--- server.py
+++ server.py
@@ -23,8 +23,10 @@ class Application(cyclone.web.Application):
         
         mongo_uri = os.environ.get('MONGOLAB_URI', False)
         if mongo_uri:
+            log.msg(mongo_uri)
             regex = re.compile("^(?P<scheme>.*):\/\/(?P<user>.*):(?P<password>.*)@(?P<host>.*):(?P<port>\d*)\/(?P<database>.*)$")
             creds = regex.search(mongo_uri).groupdict()
+            log.msg(creds)
             mongo = txmongo.lazyMongoConnectionPool(host=creds['host'], port=creds['port'])
             db = mongo[creds['database']]
             db.auth(creds['user'], creds['password'])
